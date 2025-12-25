9d4cd1479ecd45ed7391f4907fe582b8dd366d0e
Jeyamathura <jeyamathurajeyakaneshan@gmail.com>
2025-12-25 18:23:48 +0530

    Add core app and switch database to MySQL
    Registered the 'core' app in INSTALLED_APPS and updated the default database configuration from SQLite to MySQL with appropriate connection settings and options.

--- garage_backend/garage_backend/settings.py
+++ garage_backend/garage_backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -74,11 +75,18 @@ WSGI_APPLICATION = 'garage_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'garage_service_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
-}
-
+}   
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
