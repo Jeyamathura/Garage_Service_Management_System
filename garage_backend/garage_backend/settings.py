a421591c0a9c3476d672e28e036c24474e31157c
Jeyamathura <jeyamathurajeyakaneshan@gmail.com>
2026-01-04 00:48:55 +0530

    Update in backend
    update urls.py.
    Install django-cors-headers, then add corsheaders in INSTALLED_APPS.
    Add 'corsheaders.middleware.CorsMiddleware' in MIDDLEWARE.
    Allow localhost:3000 to access Django API

--- garage_backend/garage_backend/settings.py
+++ garage_backend/garage_backend/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'core',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 AUTH_USER_MODEL = 'core.User'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Allow localhost:3000 to access Django API
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
--- garage_backend/garage_backend/urls.py
+++ garage_backend/garage_backend/urls.py
@@ -1,15 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from core.views import MyTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # JWT AUTH (STANDARD)
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # JWT AUTH
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # API ROUTES
