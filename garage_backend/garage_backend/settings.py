0767197978b7b29573fae4cb65df29666e71c048
Jeyamathura <jeyamathurajeyakaneshan@gmail.com>
2026-01-10 13:02:12 +0530

    Update settings.py

--- garage_backend/garage_backend/settings.py
+++ garage_backend/garage_backend/settings.py
@@ -43,6 +43,24 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+from datetime import timedelta
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+}
+
 AUTH_USER_MODEL = 'core.User'
 
 MIDDLEWARE = [
