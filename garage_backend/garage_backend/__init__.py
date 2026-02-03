11c3dd2f18475c4ff6d86ff899a199d957fc5b30
Jeyamathura <141611323+Jeyamathura@users.noreply.github.com>
2026-02-04 01:06:54 +0530

    Update garage_backend\garage_backend\__init__.py
    updated with commented code, that must be uncommented, if only the case of failing the installation of MySQL client, as there is a need to install the pymysql, the code will tell the Django to use the pymysql.

---
+++ garage_backend/garage_backend/__init__.py
@@ -0,0 +1,6 @@
+# This file is used to register the pymysql driver with Django.
+# It is not needed if you are using mysqlclient.
+# Uncomment the following lines if you are using pymysql:
+
+#import pymysql
+#pymysql.install_as_MySQLdb()
